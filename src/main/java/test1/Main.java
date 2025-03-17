7db374d54ff86dfba98e0dfeff3da1fffc48d7d9
Ashwin Sebastian <ashwinsebastian76@gmail.com>
2025-03-17 21:02:12 +1100

    asdfasdf  asdfasdfasdfsdfasdfasdfsadfasdf
    Signed-off-by: Ashwin Sebastian <ashwinsebastian76@gmail.com>

---
+++ src/main/java/test1/Main.java
@@ -0,0 +1,29 @@
+package test1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.setProperty(
+			"webdriver.chrome.driver",
+			"/Users/ashwin/Documents/chromedriver-mac-arm64/chromedriver");
+		
+		WebDriver driver = new ChromeDriver(); 
+		System.out.println(driver);
+		
+		driver.get("https://www.google.com");
+
+		try {
+			Thread.sleep(5*1000);
+		} catch (InterruptedException e) {
+		
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		driver.close();
+
+
+	}
+}
